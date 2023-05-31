1546c1f036b5961357d05a19d9acdc6a6861554f
Raghurockystar <raghuboddu38@gmail.com>
2023-05-31 22:34:45 +0530

    changes

--- FirstApp/src/com/sathya/Addition.java
+++ FirstApp/src/com/sathya/Addition.java
@@ -3,6 +3,10 @@ package com.sathya;
 public class Addition {
 
 	public static void main(String[] args) {
+		int x=30;
+		int y=50;
+		int z=x+y;
+		System.out.println("addition two numbers"+" "+z)
 
 		
 	}
